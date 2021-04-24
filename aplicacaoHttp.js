7f62026a62861bafb29f47c5a644fc63990725e6
eduarda.rosa <eduarda.rosa@jsl.com.br>
2021-04-24 11:58:17 -0300

    Primeira aplicacao HTTP - v1

--- aplicacaoHttp.js
+++ aplicacaoHttp.js
@@ -3,7 +3,7 @@ let http = require('http');
 //criando um servidor http
 // O http é divido em requisicao e resposta
 http.createServer(function(requisicao, resposta){
-    resposta.end("Bem vindo ao meu site!");
+    resposta.end("<h1>Bem vindo ao meu site!</h1>");
 }).listen(8181);
 
 
