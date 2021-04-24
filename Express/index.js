70ac3ed8ce4b936a4064690524d6e83bba86eb6c
eduarda.rosa <eduarda.rosa@jsl.com.br>
2021-04-24 15:49:47 -0300

    Criando routes

--- Express/index.js
+++ Express/index.js
@@ -5,6 +5,25 @@ const express = require("express");
 // a funcao que vai carregar todo express na variavel app
 const app = express(); 
 
+//Criando uma route 
+app.get('/', function(req,res){
+    //Devolvendo uma resposta - sempre tem que devolver uma resposta.
+    res.send('Route principal');
+
+});
+
+app.get('/galeria', function(req,res){
+    //Devolvendo uma resposta - sempre tem que devolver uma resposta.
+    res.send('Route galeria');
+
+});
+
+app.get('/blog', function(req,res){
+    //Devolvendo uma resposta - sempre tem que devolver uma resposta.
+    res.send('Route blog');
+
+});
+
 //Funcao para abrir um servidor Express
 // 4000 numero da porta que vai rodar o servidor
 app.listen(4000,function(erro){
